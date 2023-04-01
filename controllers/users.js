291daaf7ace98f6fd818b159dfa48963289213b7
Urszula Molska <urszula_molska@wp.pl>
2023-04-02 00:01:39 +0200

    hw-05

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,34 @@
 const { User, hashPassword } = require("../models/user.js");
+const path = require("path");
+const createError = require("http-errors");
+const fs = require("fs").promises;
+var gravatar = require("gravatar");
+const Jimp = require("jimp");
+const NewStoreImage = path.join(process.cwd(), "public/avatars");
 
-const createUser = async (email, password) => {
+const createUser = async (email, password, filePath) => {
   const hashedPassword = hashPassword(password);
 
+  const avatar = gravatar.profile_url(email, {
+    protocol: "https",
+    format: "jpg",
+  });
+  console.log("avatar", avatar);
+  const ImgName = path.basename(avatar);
+  const newFilePath = path.join(NewStoreImage, ImgName);
+  console.log("newFilePath", newFilePath);
+
+  Jimp.read(filePath, (err, pict) => {
+    if (err) throw err;
+    pict.resize(250, 250).quality(80).write(newFilePath);
+  });
+
   try {
-    const user = new User({ email, password: hashedPassword });
+    const user = new User({
+      email,
+      password: hashedPassword,
+      avatarURL: avatar,
+    });
 
     user.save();
     return user;
