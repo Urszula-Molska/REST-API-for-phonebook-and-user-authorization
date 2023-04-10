6d7bb23849583ceaecea537421eeea396a6ae065
Urszula Molska <urszula_molska@wp.pl>
2023-04-10 18:37:16 +0200

    update

--- app.js
+++ app.js
@@ -29,16 +29,14 @@ app.use("/avatars", express.static(path.join(__dirname, "./public/avatars")));
 
 app.use("/public", express.static("public"));
 
-/*app.use((req, res) => {
+/* app.use((req, res) => {
   res.status(404).json({ message: "Not found !!!!" });
-});
+}); */
 
-app.use((err, req, res, next) => {
+/* app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
-});*/
+}); */
 
-//multer
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
